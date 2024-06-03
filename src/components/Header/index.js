fbbabaaf2c84ed9eb52d5c3392adbda79ab0b1cf
beasb <beatriz.sborges4@senacsp.edu.br>
2024-06-03 17:20:12 -0300

    aula dia 03

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import Logo from "../Logo";
+import OptionsHeader from "../OptionsHeader";
+import IconeHeader from "../IconesHeader";
+
+import "./style.css";
+
+function Header(){
+return (
+<header className='App-header'>
+<Logo />
+<OptionsHeader />
+<IconeHeader />
+
+</header>
+);
+}
+
+export default Header;
+
